724c197b3b8ca7eb57fe4add5fcc8173e75f626c
A1ej0 <122892151+A1ej0@users.noreply.github.com>
2024-04-10 14:40:48 -0500

    pi pico

---
+++ BLUEPILL/main.py
@@ -0,0 +1,23 @@
+from time import sleep
+from ulora import LoRa, ModemConfig, SPIConfig
+
+# Lora Parameters
+RFM95_RST = 4
+RFM95_SPIBUS = SPIConfig.rp2_0
+RFM95_CS = 2
+RFM95_INT = 21
+RF95_FREQ = 433.0
+RF95_POW =  20
+CLIENT_ADDRESS = 1
+SERVER_ADDRESS = 2
+
+# initialise radio
+lora = LoRa(RFM95_SPIBUS, RFM95_INT, CLIENT_ADDRESS, RFM95_CS, reset_pin=RFM95_RST, freq=RF95_FREQ, tx_power=RF95_POW, acks=False)
+
+
+# loop and send data
+while True:
+    lora.send("This is a test message", SERVER_ADDRESS)
+    print("sent")
+    sleep(1)
+
